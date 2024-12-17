bce251bd9afdfef5a098d32124976cc56b703754
Madis-Julius <estonira@gmail.com>
2024-12-17 23:54:33 +0200

    Requires authentication for mainpage

--- src/js/router.js
+++ src/js/router.js
@@ -4,12 +4,14 @@ import SignUp from '@/components/SignUp.vue';
 import AddPostComponent from '@/components/AddPostComponent.vue';
 import PostPage from '@/components/PostPage.vue';
 import CreateUser from '@/components/CreateUser.vue';
+import auth from './auth';
 
 const routes = [
   {
     path: '/',
     name: 'MainContent',
     component: MainContent,
+    meta: { requiresAuth: true }, 
   },
   {
     path: '/signup',
@@ -20,12 +22,14 @@ const routes = [
     path: '/post',
     name: 'Post',
     component: AddPostComponent,
+    meta: { requiresAuth: true },
   },
   {
     path: '/post/:id',
     name: 'PostPage',
     component: PostPage,
     props: true,
+    meta: { requiresAuth: true },
   },
   {
     path: '/register',
@@ -37,6 +41,18 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(),
   routes,
+});
+
+// Navigation guard to check authentication
+router.beforeEach(async (to, from, next) => {
+  const requiresAuth = to.matched.some(record => record.meta.requiresAuth);
+  const isAuthenticated = await auth.authenticated(); // Check authentication status
+
+  if (requiresAuth && !isAuthenticated) {
+    next('/signup'); // Redirect to login page if not authenticated
+  } else {
+    next(); // Proceed to the route
+  }
 });
 
 export default router;
